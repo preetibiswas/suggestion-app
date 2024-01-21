e64aeb3f45ee279497803cef3649745264dcf959
Preeti biswas <preeti.biswas@brovitech.com>
2024-01-21 19:37:31 +0530

    first-commit

---
+++ src/components/Feedbackitem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Feedbackitem = () => {
+  return (
+    <div className='card'>
+        <div className='num-display'>10</div>
+        <div className='text-display'>this is aexpamle of number display</div>
+    </div>
+  )
+}
+
+export default Feedbackitem
